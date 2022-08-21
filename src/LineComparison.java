d3ee241b5d137cd0ab7650586001266c3737d338
rakibbiswas1 <rakibbiswas.official@gmail.com>
2022-08-21 22:57:47 +0530

    checking lenghth

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -1,13 +1,13 @@
 public class LineComparison {
     public static void main(String[] args) {
-      //  int x1,y1,x2,y2;
-      //  double lengthOfline;
+        int x1,y1,x2,y2;
+        double lengthOfline;
         System.out.println("Welcome to Line Comparison Computation");
-     //   x1 = 2;
-    //    y1 = 2;
-    //    x2 = 5;
-     //   y2 = 5;
-     //   lengthOfline=(Math.sqrt( Math.pow((x2-x1),2) + Math.pow((y2-y1),2) ));
-    //    System.out.println("Length of line = "+lengthOfline);
+        x1 = 2;
+        y1 = 2;
+        x2 = 5;
+        y2 = 5;
+        lengthOfline=(Math.sqrt( Math.pow((x2-x1),2) + Math.pow((y2-y1),2) ));
+        System.out.println("Length of line = "+lengthOfline);
     }
 }
